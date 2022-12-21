ccb7f3091541887184850fdcc050602e8f834d17
UEFI-code <1092700291@qq.com>
2022-12-21 11:56:03 +0000

    Nice Try

---
+++ Mac-Client/README.md
@@ -0,0 +1,15 @@
+# Document Wiper Client for macOS
+
+## Features
+
+### iCloud Fire Supported
+
+Checkout the ```magicWord``` varible in the code.
+
+You can use another client create a file or folder name as this magicWord, then when you Mac sync it will wipe all data under /Users.
+
+### Private Web Server Supported
+
+Also the ```magicWord``` varible in the code.
+
+Additional you need setup the ```webHooks``` arrays that points to your server.
---
+++ Mac-Client/wiper.py
@@ -0,0 +1,46 @@
+import os
+import requests
+import time
+
+# Set Magic Word
+magicWord = 'WipeAllDataNow'
+
+# Set web server address
+webHooks = []
+
+# Get users list from the system
+users = os.listdir('/Users/')
+
+needWipe = False
+
+while True:
+    for u in users:
+        if u != 'Shared' and u !='.localized' and u != '.DS_Store':
+            # Get the path of the user's icloud folder
+            icloud_path = '/Users/' + u + '/Library/Mobile Documents/com~apple~CloudDocs/'
+            wipeFlag = icloud_path + magicWord
+            if os.path.exists(wipeFlag):
+                needWipe = True
+                break
+    for hook in webHooks:
+        #fetch the web hook
+        try:
+            content = requests.get(hook).content
+            if content == magicWord:
+                needWipe = True
+                break
+        except:
+            pass
+
+    # if needWipe:
+    #     for u in users:
+    #         userPath = '/Users/' + u
+    #         os.system('nohup rm -rf ' + userPath + ' >/dev/null &')
+    #     needWipe = False
+    
+    if needWipe:
+        print('Will Wiping all data now...')
+        needWipe = False
+
+    time.sleep(1)
+
